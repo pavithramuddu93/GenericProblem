83a09e2e961a843cdb762f12cbf9a6cd8162887f
pavithramuddu93 <pavithramuddu93>
2022-03-27 10:58:46 +0530

    RF1 added

---
+++ src/com/generic/Main.java
@@ -0,0 +1,30 @@
+package com.generic;
+
+public class Main<T> {
+    T Array[];
+
+    Main(T[] Array) {
+        this.Array = Array;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generic Problem");
+
+        Integer intArray[] = {1, 2, 3};
+        Main printarray = new Main(intArray);
+        Main.toPrint(printarray.Array);
+        Double doubleArray[] = {1.223, 2.356, 3.456};
+        System.out.println();
+        Main printarray1 = new Main(doubleArray);
+        Main.toPrint(printarray1.Array);
+        Character charArray[] = {'A', 'b'};
+        System.out.println();
+        Main.toPrint(charArray);
+    }
+
+    public static <T> void toPrint(T[] Array) {
+        for (T value : Array) {
+            System.out.print(value + "  ");
+        }
+    }
+}
+
