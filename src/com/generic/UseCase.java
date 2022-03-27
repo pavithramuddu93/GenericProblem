3ef710ab4b5244177931f6ec262b97db615b7edb
pavithramuddu93 <pavithramuddu93>
2022-03-27 11:13:39 +0530

    UC1 added

--- src/com/generic/UseCase.java
+++ src/com/generic/UseCase.java
@@ -1,39 +1,18 @@
 package com.generic;
 
-public class UseCase<T extends Comparable<T>> {
-    T x;
-    T y;
-    T z;
-
-    public UseCase(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
+public class UseCase {
     public static void main(String[] args) {
 
         Integer xint = 65, yint = 85, zint = 22;
-        new UseCase(xint, yint, zint).testmaximum();
-        Float xf = 9.7f, yf = 17.66f, zf = 55.5f;
-        new UseCase(xf, yf, zf).testmaximum();
-        String xs = "Pear", ys = "Apple", zs = "Orange";
-        new UseCase(xs, ys, zs).testmaximum();
-    }
-
-    public static <T extends Comparable<T>> T testmaximum(T x, T y, T z) {
-        T max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
+        int max = xint;
+        if (yint.compareTo(max) > 0) {
+            max = yint;
         }
-        if (z.compareTo(max) > 0) {
-            max = z;
+        if (zint.compareTo(max) > 0) {
+            max = zint;
         }
-        System.out.println(max + " is maximum");
-        return null;
-    }
+        System.out.println(max + " is max number");
 
-    public T testmaximum() {
-        return UseCase.testmaximum(x, y, z);
+
     }
 }
